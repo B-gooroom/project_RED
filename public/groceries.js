c3694df668137ac8239c4728db14100e44dad314
sean <j.sean1316@gmail.com>
2021-05-28 19:18:28 +0900

    groceries(add item(move))

--- public/groceries.js
+++ public/groceries.js
@@ -20,12 +20,19 @@ const groceriesCreate = function () {
 };
 
 const itemsCreate = function (index) {
-  const item = {
+  // const item = {
+  //   name: document.getElementsByName('groceries-name')[index].innerHTML,
+  //   enter: document.getElementsByName('groceries-enter')[index].innerHTML,
+  //   expire: document.getElementsByName('groceries-expire')[index].value
+  // };
+  debugger
+  const groceryUpdate = {}
+  groceryUpdate[document.getElementsByName('groceries-key')[index].value] = {
     name: document.getElementsByName('groceries-name')[index].innerHTML,
     enter: document.getElementsByName('groceries-enter')[index].innerHTML,
     expire: document.getElementsByName('groceries-expire')[index].value
   };
-  axios.post('https://be-gooroom-default-rtdb.firebaseio.com/items.json', item).then(function (response) {
+  axios.patch('https://be-gooroom-default-rtdb.firebaseio.com/items.json', groceryUpdate).then(function (response) {
     console.log('Done itemsCreate', response.data);
   });
 };
@@ -36,17 +43,22 @@ const groceriesRead = function () {
     let groceries = response.data;
     const tbody = document.getElementById('tbody-groceries');
     tbody.innerHTML = '';
+    for (let key in groceries) {
+      const grocery = groceries[key];
+      grocery.k = key
+    }
     groceries = _.orderBy(groceries, orderName, orderType);
     console.log(groceries);
     let index = 0;
     for (let key in groceries) {
+      debugger
       const grocery = groceries[key];
       const tr = document.getElementById('tr-template-groceries').cloneNode(true);
       tbody.appendChild(tr);
       document.getElementsByName('groceries-name')[index].innerHTML = grocery.name;
       document.getElementsByName('groceries-enter')[index].innerHTML = grocery.enter;
       document.getElementsByName('groceries-expire')[index].value = grocery.expire;
-      document.getElementsByName('groceries-key')[index].value = key;
+      document.getElementsByName('groceries-key')[index].value = grocery.k;
       document.getElementsByName('groceries-expire')[index].index = index;
       document.getElementsByName('groceries-delete')[index].index = index;
       document.getElementsByName('items-create')[index].index = index;
